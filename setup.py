e08e4ce3ef6a619bca94f6cd6f1775cfafc5e89a
Raphaël Vinot <raphael@vinot.info>
2016-04-15 16:32:23 +0200

    Bump version
    Fix #2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pyeupi',
-    version='0.9',
+    version='1.0',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
